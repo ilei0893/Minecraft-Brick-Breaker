1e9a81810ed9715e14a672bdc603ee274a74b78f
Ivan Lei <ilei0893@gmail.com>
2020-05-11 15:12:40 -0400

    formatting

--- BrickBreaker/src/brickBreaker/Main.java
+++ BrickBreaker/src/brickBreaker/Main.java
@@ -3,18 +3,26 @@ package brickBreaker;
 import java.io.IOException;
 import javax.swing.JFrame;
 
-public class Main {
 
-	public static void main(String[] args) throws IOException {
-		JFrame obj = new JFrame();
-		Gameplay gamePlay = new Gameplay();
+public class Main {
+	
+	public static JFrame obj;
+	public static Gameplay gamePlay;
+	
+	public static void runGame()
+	{
+		obj = new JFrame();
+		gamePlay = new Gameplay();
 		obj.setBounds(10, 10, 700, 600); // Create Swing GUI
 		obj.setTitle("Brick Breaker");
 		obj.setResizable(false);
 		obj.setVisible(true);
 		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		obj.add(gamePlay);
-
 	}
+	
 
+	public static void main(String[] args) throws IOException {
+		runGame();
+	}
 }
